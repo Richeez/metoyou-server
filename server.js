9f92d2ab7607f4203841850d099f2e3e894ef66d
Richard Saturday <96580640+Richeez@users.noreply.github.com>
2023-07-26 18:12:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 
 
+class MyEmitter extends EventEmitter { };
+//? initialize object
+const myEmitter = new MyEmitter();
+
+//? add listener for log events
+myEmitter.on('log', (msg) => logEvents(msg))
+
+setTimeout(() => {
+    //? Emmit events
+    myEmitter.emit('log', 'log event emmitted successfully')
+
+}, 2000);
+
+
 
