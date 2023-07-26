d9978974c400d2429a4ffe84c06dfe9129cad383
Richard Saturday <96580640+Richeez@users.noreply.github.com>
2023-07-26 03:02:12 +0100

    Update allowedOrigins.js to recieve all site

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
-const allowedOrigins = [
-    'https://metoyou.vercel.app/',
-    'https://metoyou-server.vercel.app/',
-    // 'http://127.0.0.1:5173'
+const allowedOrigins = `*`
+//     [
+//     'https://metoyou.vercel.app/',
+//     'https://metoyou-server.vercel.app/',
+//     // 'http://127.0.0.1:5173'
 
-];
+// ];
 module.exports = allowedOrigins
