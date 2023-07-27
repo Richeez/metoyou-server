cf3a43a56771d6cdf9d06cbfb2fda352db4a1942
Richard Saturday <96580640+Richeez@users.noreply.github.com>
2023-07-27 12:30:29 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,9 @@
 const allowedOrigins =
-    ["https://metoyou.vercel.app/"];
-module.exports = allowedOrigins
+    ['https://metoyou.vercel.app/', 
+     'www.metoyou.vercel.app/',
+     'http://127.0.0.1:5500',
+    'http://localhost:4000',
+    'http://localhost:3000'];
+
+
+module. Exports = allowedOrigins
