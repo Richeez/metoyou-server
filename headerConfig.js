22d972c139b5c0acb150caad8244f1147634755b
Richard Saturday <96580640+Richeez@users.noreply.github.com>
2023-07-27 05:04:07 +0100

    Update headerConfig.js

--- headerConfig.js
+++ headerConfig.js
@@ -8,10 +8,10 @@ module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
 
     // Allow specific HTTP headers
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Origin,X-Requested-With, Accept, Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Accept, Content-Type');
 
     // Set how long the preflight request can be cached (in seconds)
-    res.setHeader('Access-Control-Max-Age', '3600');
+    // res.setHeader('Access-Control-Max-Age', '3600');
 
     // Handle preflight requests (OPTIONS method)
     if (req.method === 'OPTIONS') {
