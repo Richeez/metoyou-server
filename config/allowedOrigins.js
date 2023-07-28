cc26cc13d1526a007ab447a5f790218b97d3dc00
DevRichard <saturdayrichard98@gmail.com>
2023-07-28 04:29:40 +0100

    CORS FIX #7

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins =
     ['https://metoyou.vercel.app',
         'https://www.metoyou.vercel.app',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
     ];
 
 
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,9 +1,14 @@
 const allowedOrigin = require('./allowedOrigins')
 const corsOptions = {
     origin: (origin, callback) => {
-        allowedOrigin.indexOf(origin) !== -1 || !origin
-            ? callback(null, true)
-            : callback(new Error('Not allowed by CORS'))
+        if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+
+        } else {
+            callback(new Error('Not allowed by CORS'))
+
+        }
+
     },
     credentials: true,
     optionsSuccessStatus: 200
